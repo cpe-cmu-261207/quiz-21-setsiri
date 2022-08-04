d8077fbaa851d74edbc97f7ec5216ccb71962aff
setsiri <76035222+setsiri@users.noreply.github.com>
2022-08-04 14:10:42 +0700

    push

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const spanGenderIcon = document.getElementById("span-gender-icon");
+const divUserCard = document.getElementById("div-user-card");
+const imgProfile = document.getElementById("img-profile");
+const pName = document.getElementById("p-name");
+const pEmail = document.getElementById("p-email");
+const pAddress = document.getElementById("p-address");
+const btnRandom = document.getElementById("btn-random");
+const divLoadingCard = document.getElementById("div-loading-card");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgProfile.src = resp.data.result[].picture.large;
 }
+
+divUserCard.style.display = "none";
+
+imgProfile.onload = () => {
+  divUserCard.style.display = "";
+};
